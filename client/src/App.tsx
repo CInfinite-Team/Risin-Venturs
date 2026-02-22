b541b7384f2ad85aa4852120c0fbac15946e5d93
gajju44 <gajjunaphade@gmail.com>
2026-02-22 18:33:21 +0530

    fixed not scroll to top when navigating

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import ScrollToTopOnMount from "@/components/ScrollToTopOnMount";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import About from "@/pages/about";
@@ -67,6 +68,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <ScrollToTopOnMount />
         <Toaster />
         <Router />
       </TooltipProvider>
